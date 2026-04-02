163135a02c4966e429e1b282cd02d4e1b23f6251
Queenie-101 <queenestheradekanmi@gmail.com>
2026-04-02 22:00:06 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        String b = "Susan";
+        float c = 23.7f;
+        boolean d = true;
+        System.out.println("variable a=" + a);
+        System.out.println("variable b=" + b);
+        System.out.println("variable c=" + c);
+        System.out.println("variable d=" + d);
+    }
+}
